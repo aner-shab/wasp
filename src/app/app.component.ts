c4175d9ccc9e5ddc0bf899d56a5b9fdeab744496
aner-shab <aner.shab@gmail.com>
2020-03-13 13:05:14 +0200

    POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,167 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+enum Guess {
+  None = 0,
+  Correct = 1,
+  Wrong = 2
+}
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class="hex-row">
-    <div class="hex">
-      <app-cell [placeholder]="true" ></app-cell>
+  <div class="beehive">
+    <div class="query animated" [ngClass]="{ 
+      'shake': isGuess === 2, 'bounceOutUp': isGuess === 1}">{{currentQuery}} 
+      <span class="blinking-cursor" [hidden]="isGuess !== 0">|</span>
     </div>
-    <div class="hex even">
-      <app-cell [letter]="lettersInRotation[0]" (clicked)="onLetterClicked($event)"></app-cell>
+    <div class="hex-row">
+      <div class="hex">
+        <app-cell [placeholder]="true" ></app-cell>
+      </div>
+      <div class="hex even">
+        <app-cell [letter]="lettersInRotation[0]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+    </div>
+    <div class="hex-row">
+      <div class="hex">
+        <app-cell [letter]="lettersInRotation[1]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+      <div class="hex even">
+        <app-cell [letter]="lettersInRotation[2]" [isKey]="true" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+      <div class="hex">
+        <app-cell [letter]="lettersInRotation[3]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+    </div>
+    <div class="hex-row">
+      <div class="hex">
+        <app-cell [letter]="lettersInRotation[4]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+      <div class="hex even">
+        <app-cell [letter]="lettersInRotation[5]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
+      <div class="hex">
+        <app-cell [letter]="lettersInRotation[6]" (clicked)="onLetterClicked($event)"></app-cell>
+      </div>
     </div>
   </div>
-  <div class="hex-row">
-    <div class="hex">
-      <app-cell [letter]="lettersInRotation[1]" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
-    <div class="hex even">
-      <app-cell [letter]="lettersInRotation[2]" [isKey]="true" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
-    <div class="hex">
-      <app-cell [letter]="lettersInRotation[3]" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
+
+  <div class="buttons-bar">
+    <button class="game-button" (click)="onBackspaceClicked()">Delete</button>
+    <button class="game-button" (click)="onJumbleClicked()">Jumble</button>
+    <button class="game-button" (click)="onEnterClicked()">Enter</button>
   </div>
-  <div class="hex-row">
-    <div class="hex">
-      <app-cell [letter]="lettersInRotation[4]" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
-    <div class="hex even">
-      <app-cell [letter]="lettersInRotation[5]" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
-    <div class="hex">
-      <app-cell [letter]="lettersInRotation[6]" (clicked)="onLetterClicked($event)"></app-cell>
-    </div>
+
+  <div class="buttons-bar">
+    <button class="game-button reset" (click)="onResetClicked()">Reset</button>
+  </div>
+
+  <div class="hint">You have found {{wordsFound.length}} words out of {{wordsBank.length}}</div>
+  <div class="words-found">
+    <span *ngFor="let word of wordsFound">{{word}}</span>
   </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'wasp';
   lettersInRotation = [];
-    
+  wordsBank = [];
+  wordsFound = [];
+  currentQuery = '';
+  loaded = false;
+
+  isGuess: Guess = Guess.None;
+
+  constructor(private http: HttpClient){
+  }  
+
   ngOnInit(){
-    this.initializeLetters();
+    this.initializeLetters();   
   }
 
   initializeLetters(){
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this.loaded = false;
+    this.wordsFound = [];
+    this.currentQuery = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     let letters = [];
     while (letters.length < 7){
       const alphaLength = alphabet.length;
-      let randomIndex = Math.floor(Math.random() * alphaLength);
+      const randomIndex = Math.floor(Math.random() * alphaLength);
       letters.push(alphabet.splice(randomIndex,1));
     }
-    this.lettersInRotation = letters;
+    if (this.hasVowel(letters.join(''))){
+      this.lettersInRotation = letters;
+      this.loadListOfWords();
+    }else{
+      this.initializeLetters();
+    }
   }
-  onLetterClicked(event){
-    console.log(event);
+
+  hasVowel(letters){
+    if (letters.includes('a')
+    || letters.includes('e')
+    || letters.includes('i')
+    || letters.includes('o')
+    || letters.includes('u')){
+      return true;
+    }
+    return false;
+  }
+  async loadListOfWords(){
+    console.log(this.lettersInRotation);
+    let words = await this.http.get(
+    `https://api.yourdictionary.com/wordfinder/v1/unscrambler?dictionary=US%2CWWF&dictionary_opt=YDR&limit=0&tiles=${this.lettersInRotation}`)
+    .toPromise();
+    // TODO this is scrabble! we don't have double-letter support
+    this.wordsBank = (<any>words).data._items.map(x=> x.word).filter(x=> x.length > 3);
+    console.log(this.wordsBank);
+    if (this.wordsBank.length > 2){
+      this.loaded = true;
+    }else{
+      this.initializeLetters();
+    }
+  }
+
+  onLetterClicked(letter){
+    this.currentQuery += letter;
+  }
+
+  onJumbleClicked(){
+    const keyLetter = this.lettersInRotation[2];
+    for (let i = this.lettersInRotation.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.lettersInRotation[i], this.lettersInRotation[j]] = [this.lettersInRotation[j], this.lettersInRotation[i]];
+    }
+    const indexOfKeyLetter = this.lettersInRotation.findIndex((key)=> key === keyLetter);
+    if (indexOfKeyLetter !== 2){
+      const letterInKeyIndex = this.lettersInRotation[2];
+      this.lettersInRotation[2] = keyLetter;
+      this.lettersInRotation[indexOfKeyLetter] = letterInKeyIndex;
+    }
+  }
+
+  onResetClicked(){
+    if (confirm("Are you sure you want to reset?")){
+      this.initializeLetters();
+    }
+  }
+
+  onBackspaceClicked(){
+    this.currentQuery = this.currentQuery.slice(0,-1);
+  }
+
+  onEnterClicked(){
+    if (this.wordsBank.find(word=> word === this.currentQuery)){
+      this.wordsFound.push(this.currentQuery);
+      this.isGuess = Guess.Correct;
+    }else{
+      this.isGuess = Guess.Wrong;
+    }
+    setTimeout(()=> {
+      this.isGuess = Guess.None;
+      this.currentQuery = '';
+    }, 1000);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CellComponent } from './cell/cell.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { CellComponent } from './cell/cell.component';
     CellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
